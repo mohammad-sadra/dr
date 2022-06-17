8c97556e06cc2c85426d9960ec58bf5c1c6cc843
mohammad-sadra <63974514+mohammad-sadra@users.noreply.github.com>
2022-06-17 14:11:05 +0430

    update

--- doctor/views.py
+++ doctor/views.py
@@ -5,11 +5,17 @@ from rest_framework import status
 from django.http import HttpResponse
 from .models import doctor, reserve
 from .serializers import doctoradd, doctorget, reserveadd, reserveget
+from datetime import date
+
 
 
 class getdata(api):
 
-    def get(self, request):
-        doctors = doctor.objects.all()
+    def get_doctor(self, request):
+        doctors = doctor.objects.all(reserve_date=1)
         serializer = doctoradd(doctors, many=True)
         return Response(serializer.data)
+
+
+    def get_reserve(self, request):
+        reserves = reserve.object.filter()
